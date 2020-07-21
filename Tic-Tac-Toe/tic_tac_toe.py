3176d73e6e736516648e6942e8c32a8a42ea2b6a
Tejas A. Sheth <52593382+TejasSheth104@users.noreply.github.com>
2020-07-21 18:18:23 +0530

    created multiple functions.

--- Tic-Tac-Toe/tic_tac_toe.py
+++ Tic-Tac-Toe/tic_tac_toe.py
@@ -18,7 +18,42 @@ def draw_board(board):
     print('\t ' + board['7'] + ' | ' + board['8'] + '  | ' + board['9'])
 
 
-def play_user():
+def clear(count):
+    if count == 0:
+        print('\n\n\t-#- -#- -#- -#-\n')
+        print('Lets Start...')
+        for keys, value in board.items():
+            if board[keys] == value:
+                board[keys] = ' '
+        draw_board(board)
+
+
+def winning_condition(count, chance):
+    if count >= 4:
+        if (board['1'] == board['2'] and board['2'] == board['3']) or \
+            (board['4'] == board['5'] and board['5'] == board['6']) or \
+            (board['7'] == board['8'] and board['8'] == board['9']) or \
+            (board['1'] == board['4'] and board['4'] == board['7']) or \
+            (board['2'] == board['5'] and board['5'] == board['8']) or \
+            (board['3'] == board['6'] and board['6'] == board['9']) or \
+            (board['1'] == board['5'] and board['5'] == board['9']) or \
+            (board['3'] == board['5'] and board['5'] == board['7']) or \
+            (board['7'] == board['8'] and board['8'] == board['9']):
+            print('Winner is - ', chance)
+            draw_board(board)
+            print("\nGame Over.\n")
+            return 1
+        else:
+            return 0
+
+
+def switch(chance):
+    if chance == 'X':
+        chance = 'O'
+    else:
+        chance = 'X'
+
+def play_user_v_user():
     
     chance = 'X'
     count = 0
@@ -27,13 +62,7 @@ def play_user():
         print('\n')
         draw_board(board)
 
-        if count == 0:
-            print('\n\n\t-#- -#- -#- -#-\n')
-            print('Lets Start...')
-            for keys, value in board.items():
-                if board[keys] == value:
-                    board[keys] = ' '
-            draw_board(board)
+        clear(count)
 
         print('\nIts your Turn,', chance)
         move = input('Position at ? place  -> ')
@@ -44,40 +73,23 @@ def play_user():
             print('This Position is ALREADY OCCUPIED, \nplease RETRY...')
             continue
 
-
-        if count >= 4:
-            if (board['1'] == board['2'] and board['2'] == board['3']) or \
-                (board['4'] == board['5'] and board['5'] == board['6']) or \
-                (board['7'] == board['8'] and board['8'] == board['9']) or \
-                (board['1'] == board['4'] and board['4'] == board['7']) or \
-                (board['2'] == board['5'] and board['5'] == board['8']) or \
-                (board['3'] == board['6'] and board['6'] == board['9']) or \
-                (board['1'] == board['5'] and board['5'] == board['9']) or \
-                (board['3'] == board['5'] and board['5'] == board['7']) or \
-                (board['7'] == board['8'] and board['8'] == board['9']):
-                print('Winner is - ')
-                draw_board(board)
-                print("\nGame Over.\n")
-                break 
+        check = winning_condition(count, chance)
+        if check == 1:
+            break
 
         if count == 9:
-            print('Its a Draw...') 
+            print('Its a TIE...') 
 
-        if chance == 'X':
-            chance = 'O'
-        else:
-            chance = 'X'
+        switch(chance)
 
         count += 1 
-
-play_user()
 
 while True:
     print('\n\tTIC TAC TOE.')
     print('\n\tPLAY AS - ')
     print('\t1. PLAYER1 vs COMPUTER')
     print('\t2. PLAYER1 vs PLAYER2')
-    print('\t3. COMPUTER vs PLAYER2')
+    print('\t3. COMPUTER vs COMPUTER')
     print('\t0. EXIT')
     try:
         choice = int(input('Enter - '))
@@ -89,7 +101,7 @@ while True:
     elif choice == 1:
         break
     elif choice == 2:
-        play_user()
+        play_user_v_user()
     elif choice == 3:
         break
     
