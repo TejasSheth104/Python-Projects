28af0e1249fd5a77bfbdbccc6c0fb7f5594d74c0
Tejas A. Sheth <52593382+TejasSheth104@users.noreply.github.com>
2020-07-23 16:17:09 +0530

    number system.

---
+++ Number-System/number-system.py
@@ -0,0 +1,10 @@
+# write a python program to -
+# demonstarte interconversion of different number systems.
+# 1.) demical <=> binary
+# 2.) demical <=> octal
+# 3.) demical <=> hexadecimal
+# 4.) octal <=> binary
+# 5.) octal <=> hexadecimal
+# 6.) hexadecimal <=> binary
+
+
